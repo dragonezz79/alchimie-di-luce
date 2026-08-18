fc7375630e69f3f149f345cdee8cd025e0ea4105
dragonezz79 <nicitacarmelo@yahoo.com>
2026-08-18 12:07:27 +0200

    Route home to new product journey page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import HomeSalesPage from './HomeSalesPage.jsx';
 import { MapSalesPage, ProductsSalesPage } from './SpecialPages.jsx';
 
 const path = window.location.pathname.replace(/\/$/, '') || '/';
-const RootPage = path === '/prodotti/mappa-di-luce'
-  ? MapSalesPage
-  : path === '/prodotti'
-    ? ProductsSalesPage
-    : App;
+const RootPage = path === '/'
+  ? HomeSalesPage
+  : path === '/prodotti/mappa-di-luce'
+    ? MapSalesPage
+    : path === '/prodotti'
+      ? ProductsSalesPage
+      : App;
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
